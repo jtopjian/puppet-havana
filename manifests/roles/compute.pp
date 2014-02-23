8bb33dd9724dbf24a2279976d26e324bed0b0b11
Joe Topjian <joe@topjian.net>
2014-02-23 10:28:38 +0100

    Compute typo

--- manifests/roles/compute.pp
+++ manifests/roles/compute.pp
@@ -1,7 +1,7 @@
 class havana::roles::compute {
 
   class { 'cubbystack::repo': } ->
-  class { 'havana::common::users': } ->
+  class { 'havana::profiles::common::users': } ->
   class { 'havana::profiles::compute::nova': } ->
   class { 'havana::profiles::compute::neutron': }
 
