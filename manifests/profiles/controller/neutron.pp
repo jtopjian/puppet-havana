ff3615286744b1b8280ab941ce764c295372fb3c
Joe Topjian <joe@topjian.net>
2014-02-23 11:13:49 +0100

    linuxbridge update

--- manifests/profiles/controller/neutron.pp
+++ manifests/profiles/controller/neutron.pp
@@ -13,6 +13,17 @@ class havana::profiles::controller::neutron {
   # merge the two settings
   $linuxbridge_merged = merge($linuxbridge_settings, $vxlan)
 
+  # Fix default neutron config for ubuntu
+  case $::lsbdistid {
+    'Ubuntu': {
+      file_line { '/etc/default/neutron-server NEUTRON_PLUGIN_CONFIG':
+        path  => '/etc/default/neutron-server',
+        line  => 'NEUTRON_PLUGIN_CONFIG="/etc/neutron/plugins/linuxbridge/linuxbridge_conf.ini',
+        match => '^NEUTRON_PLUGIN_CONFIG'
+      }
+    }
+  }
+
   class {
     'cubbystack::neutron':
       settings => hiera_hash('havana::neutron::settings');
