54f7d963d8c05245708877336883c1d506c5a042
Joe Topjian <joe@topjian.net>
2014-02-23 10:42:05 +0100

    Fixing linuxbridge configuration for compute

--- manifests/profiles/compute/neutron.pp
+++ manifests/profiles/compute/neutron.pp
@@ -6,15 +6,19 @@ class havana::profiles::compute::neutron {
   $internal_device = hiera('havana::network::internal::device')
   $internal_address = getvar("ipaddress_${internal_device}")
 
-  $linuxbridge_settings = hiera('havana::neutron::plugins::linuxbridge::settings')
-  $linuxbridge_settings['vxlan/local_ip'] = $internal_address
+  $linuxbridge_settings = hiera_hash('havana::neutron::plugins::linuxbridge::settings')
 
+  # Determine the internal address for vxlan
+  $vxlan = { 'vxlan/local_ip' => $internal_address }
+
+  # merge the two settings
+  $linuxbridge_merged = merge($linuxbridge_settings, $vxlan)
 
   class {
     'cubbystack::neutron':
       settings => hiera('havana::neutron::settings');
     'cubbystack::neutron::plugins::linuxbridge':
-      settings => $linuxbridge_settings;
+      settings => $linuxbridge_merged;
   }
 
 }
